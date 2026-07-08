ba2eb5bbf18483950441210a4a2d631599849e9b
Vivek <vivek_wolves@yahoo.com>
2026-07-08 19:25:53 -0400

    commit -images

--- src/lib/gallery.ts
+++ src/lib/gallery.ts
@@ -1,12 +1,12 @@
 // Gallery images. Keep this to ~6-8 items. This is the list the admin
 // will manage from the CMS later (add/replace up to 8 photos).
 export type GalleryItem = {
-  src: string;
-  caption: string;
+	src: string;
+	caption: string;
 };
 
 export const galleryItems: GalleryItem[] = [
-  { src: "/gallery/gallery-01.png", caption: "Our delivery fleet" },
-  { src: "/gallery/gallery-02.png", caption: "In the lab & our team" },
-  { src: "/gallery/gallery-03.png", caption: "Our product & brand range" },
+	{ src: '/gallery/gallery-01.png', caption: 'Our delivery fleet' },
+	{ src: '/gallery/gallery-02.png', caption: 'Our team' },
+	{ src: '/gallery/gallery-03.png', caption: 'Our product & brand range' },
 ];
